53f0ac545650be51e3f66b7c449ecb382894d04f
Michael <Mlcorrieri@gmail.com>
2021-04-06 16:56:14 -0400

    9 outta 11 passed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,16 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
+  const projectItems = projects.map((projectObj) => {
+    return <ProjectItem key={projectObj.id} name={projectObj.name} />;
+  });
+
   console.log(projects);
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectItems}</div>
     </div>
   );
 }
